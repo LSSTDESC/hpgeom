9b5de16ea8268873de21a9d8e76e37e7c7a547ae
Eli Rykoff <erykoff@stanford.edu>
2022-06-12 12:17:16 -0700

    Better dimension handling, including scalars.

--- hpgeom/hpgeom.c
+++ hpgeom/hpgeom.c
@@ -9,8 +9,6 @@
 #include "hpgeom_utils.h"
 
 
-// add radians option...
-
 static PyObject *angle_to_pixel(PyObject *dummy, PyObject *args, PyObject *kwargs) {
     int64_t nside;
     PyObject *a_obj=NULL, *b_obj=NULL;
@@ -18,21 +16,19 @@ static PyObject *angle_to_pixel(PyObject *dummy, PyObject *args, PyObject *kwarg
     PyObject *pix_obj=NULL;
     int lonlat=1;
     int nest=1;
-    static char *kwlist[] = {"nside", "a", "b", "lonlat", "nest", NULL};
+    int degrees=1;
+    static char *kwlist[] = {"nside", "a", "b", "lonlat", "nest", "degrees", NULL};
 
-    npy_intp n_a, n_b;
-    npy_intp dims[1];
     int64_t *pixels;
     int i;
-    int status;
     double *a_data, *b_data;
     double theta, phi;
     healpix_info hpx;
     char err[ERR_SIZE];
 
-    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "lOO|pp", kwlist,
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "lOO|ppp", kwlist,
                                      &nside, &a_obj, &b_obj,
-                                     &lonlat, &nest))
+                                     &lonlat, &nest, &degrees))
         return NULL;
 
     a_arr = PyArray_FROM_OTF(a_obj, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY | NPY_ARRAY_ENSUREARRAY);
@@ -40,14 +36,27 @@ static PyObject *angle_to_pixel(PyObject *dummy, PyObject *args, PyObject *kwarg
     b_arr = PyArray_FROM_OTF(b_obj, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY | NPY_ARRAY_ENSUREARRAY);
     if (b_arr == NULL) goto fail;
 
-    /* fix these; they crash on single*/
-    n_a = PyArray_DIM((PyArrayObject *)a_arr, 0);
-    n_b = PyArray_DIM((PyArrayObject *)b_arr, 0);
+    int ndim_a = PyArray_NDIM((PyArrayObject *)a_arr);
+    int ndim_b = PyArray_NDIM((PyArrayObject *)b_arr);
 
-    /* check that n_a == n_b */
-    /* also that only 1d? */
+    if (ndim_a != ndim_b) {
+        fprintf(stdout, "Mismatched dims!\n");
+        goto fail;
+    }
+    bool is_scalar = (ndim_a == 0);
+    if (is_scalar) {
+        fprintf(stdout, "scalar!\n");
+    }
 
-    if ((pixels = (int64_t *) calloc(n_a, sizeof(int64_t))) == NULL) {
+    npy_intp a_size = PyArray_SIZE((PyArrayObject *)a_arr);
+    npy_intp b_size = PyArray_SIZE((PyArrayObject *)b_arr);
+
+    if (a_size != b_size) {
+        fprintf(stdout, "Mismatched sizes!\n");
+        goto fail;
+    }
+
+    if ((pixels = (int64_t *) calloc(a_size, sizeof(int64_t))) == NULL) {
         // raise
         goto fail;
     }
@@ -63,14 +72,14 @@ static PyObject *angle_to_pixel(PyObject *dummy, PyObject *args, PyObject *kwarg
     }
     if (!check_nside(nside, scheme, err)) {
         // raise with error string
-        fprintf(stderr, err);
+        fprintf(stderr, "%s", err);
         goto fail;
     }
     hpx = healpix_info_from_nside(nside, scheme);
 
-    for (i=0; i<n_a; i++) {
+    for (i=0; i<a_size; i++) {
         if (lonlat) {
-            if (!lonlat_to_thetaphi(a_data[i], b_data[i], &theta, &phi, true, err)) {
+            if (!lonlat_to_thetaphi(a_data[i], b_data[i], &theta, &phi, (bool) degrees, err)) {
                 // raise with err string
                 goto fail;
             }
@@ -82,16 +91,16 @@ static PyObject *angle_to_pixel(PyObject *dummy, PyObject *args, PyObject *kwarg
         pixels[i] = ang2pix(hpx, theta, phi);
     }
 
-    dims[0] = (npy_intp) n_a;
-    pix_obj = PyArray_SimpleNewFromData(1, dims, NPY_INT64, pixels);
+    if (is_scalar) {
+        pix_obj = PyLong_FromLongLong(pixels[0]);
+    } else {
+        pix_obj = PyArray_SimpleNewFromData(1, PyArray_DIMS(a_arr), NPY_INT64, pixels);
+    }
 
     /* do I free the memory from pixels? */
 
     Py_DECREF(a_arr);
     Py_DECREF(b_arr);
-
-    //dims[0] = (npy_intp) nlon;
-    //pix_obj = PyArray_ZEROS(1, dims, NPY_INT64, 0);
 
     return pix_obj;
 
@@ -103,11 +112,36 @@ static PyObject *angle_to_pixel(PyObject *dummy, PyObject *args, PyObject *kwarg
     Py_RETURN_NONE;
 }
 
+PyDoc_STRVAR(angle_to_pixel_doc,
+             "angle_to_pixel(nside, a, b, nest=True, lonlat=True, degrees=True)\n"
+             "--\n\n"
+             "Convert angles to pixels.\n"
+             "\n"
+             "Parameters\n"
+             "----------\n"
+             "nside : `int`\n"
+             "    HEALPix nside.  Must be power of 2 for nest ordering.\n"
+             "a : `np.ndarray` (N,)\n"
+             "    Longitude (radians if lonlat=False, degrees if lonlat=True and degrees=True)\n"
+             "b : `np.ndarray` (N,)\n"
+             "    Latitude or co-latitude\n"
+             "nest : `bool`, optional\n"
+             "    Use nest ordering scheme?  Default is True.\n"
+             "lonlat : `bool`, optional\n"
+             "    Use longitude/latitude instead of longitude/co-latitude (radians).  Default is True.\n"
+             "degrees : `bool`, optional\n"
+             "    If lonlat is True then this sets if the units are degrees or radians.\n"
+             "\n"
+             "Returns\n"
+             "-------\n"
+             "pix : `np.ndarray` (N,)\n"
+             "    HEALPix pixel numbers.\n"
+             );
 
 static PyMethodDef hpgeom_methods[] = {
     {"angle_to_pixel", (PyCFunction)(void(*)(void))angle_to_pixel,
      METH_VARARGS | METH_KEYWORDS,
-     "Convert angles to pixels."},
+    angle_to_pixel_doc},
     {NULL, NULL, 0, NULL}
 };
 
