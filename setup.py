ce2302ed1108d42b59882f4963a7a662d1f951ba
Eli Rykoff <erykoff@stanford.edu>
2022-06-11 20:30:49 -0700

    Add basic setup scripts, etc.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, Extension, find_packages
+import numpy
+
+
+ext = Extension(
+    "hpgeom._hpgeom",
+    [
+        "hpgeom/healpix_geom.c",
+        "hpgeom/hpgeom.c",
+    ],
+)
+
+setup(
+    name="hpgeom",
+    packages=find_packages(),
+    version="0.0.1",
+    ext_modules=[ext],
+    include_dirs=numpy.get_include(),
+)
