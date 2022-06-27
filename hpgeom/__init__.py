f38cdd9fefc078d174abc6a88796b28d18b7c708
Eli Rykoff <erykoff@stanford.edu>
2022-06-26 22:20:20 -0700

    Add __version__ to package.

--- hpgeom/__init__.py
+++ hpgeom/__init__.py
@@ -1,3 +1,14 @@
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("hpgeom")
+except PackageNotFoundError:
+    # package is not installed
+    pass
+
 from . import hpgeom
 
 from .hpgeom import *
