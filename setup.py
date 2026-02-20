4248ace680fc37106cfb5dadc040a17649b91289
Eli Rykoff <erykoff@stanford.edu>
2026-02-20 15:34:11 -0800

    Add build support for threads.

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, Extension
 import numpy
+import sys
 
 
 ext = Extension(
@@ -12,7 +13,16 @@ ext = Extension(
     ],
 )
 
+extra_link_args = []
+if sys.platform == 'win32':
+    # Windows: no pthread library needed, use native threads
+    pass
+else:
+    # Linux/macOS: link with pthread
+    extra_link_args.append('-pthread')
+
 setup(
     ext_modules=[ext],
     include_dirs=numpy.get_include(),
+    extra_link_args=extra_link_args,
 )
